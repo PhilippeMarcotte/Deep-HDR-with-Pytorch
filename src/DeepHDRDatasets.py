fcccd9e652c56b47746eebd1adf2082de686edd7
Philippe Marcotte <philippe.marcotte@polymtl.ca>
2017-11-26 22:28:01 -0500

    Bug des noms de dossiers

--- src/DeepHDRDatasets.py
+++ src/DeepHDRDatasets.py
@@ -10,23 +10,26 @@ class DeepHDRScenes(Dataset):
     def __init__(self, root):
         self.root = os.path.join(root, '')
 
+        self.scenes = next(os.walk(self.root))[1]
+
         self.num_patches = 10*49*74
         
     def __getitem__(self, index):
         scene = math.floor(index/self.num_patches)
+        scene = self.scenes[scene]
 
-        scene_labels = np.fromfile(os.path.join(self.root, str(scene), "label"), dtype='uint8')
+        scene_labels = np.fromfile(os.path.join(self.root, scene, "label"), dtype='uint8')
         scene_labels = np.reshape(scene_labels, (self.num_patches, 3, 40, 40))
         scene_labels = np.squeeze(scene_labels)        
         
-        scene_imgs = np.fromfile(os.path.join(self.root, str(scene), "imgs"), dtype='uint8')
+        scene_imgs = np.fromfile(os.path.join(self.root, scene, "imgs"), dtype='uint8')
         scene_imgs = scene_imgs.reshape((self.num_patches, 18, 40, 40))
         scene_imgs = np.squeeze(scene_imgs)
 
         return (scene_imgs, scene_labels)
 
     def __len__(self):
-        return sum(os.path.isdir(self.root + i) for i in os.listdir(self.root))
+        return len(self.scenes)
 
 class DeepHDRPatches(Dataset):
     def __init__(self, scene_imgs, scene_labels):
